ef3758f34a297486eadcae6ca7cf6d93bf013221
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-05-18 17:54:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,6 +1,6 @@
 # Kanged From @TroJanZheX
 import asyncio
-import re
+import r
 import ast
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
