568b53bc6adf1a2c369738ebaa1c7b378a25be41
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-09-24 22:58:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/YouGotMe01/EvaMaria-extra-features_mpd.git /PiroAutoFilterBot
+  git clone https://github.com/Ghostop02/EvaMaria-extra-features_mpd.git /PiroAutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PiroAutoFilterBot
